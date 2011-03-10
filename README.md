23d372235b520da03c499da589d260bfd3991999
Jacob Thornton <jacobthornton@gmail.com>
2011-03-10 10:54:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+MOOTOOLS FLOT
+=============
+
+This is a direct port of flot from jquery to mootools (+ a few plugins)...
+
+Everything is as originally wrote in v 0.6 (execept now runs on mootools).
+
+Documentation can be found here [http://flot.googlecode.com/svn/trunk/API.txt](http://flot.googlecode.com/svn/trunk/API.txt)
+
+Use at your own RISK. ;) Flot is still teh jank + and i did this port at like 3 am.
+
+OH CRAP... ONE MORE THING
+=========================
+
+Instead of overloading the dollar (like in jquery), i put flot on a flot namespace...
+
+    // So this...
+
+    $.plot({...});
+   
+    // Becomes
+
+    flot.plot({...});
+
+
+<3 @fat
---
+++ Source/moo.flot.autoscale.js
@@ -0,0 +1,48 @@
+/* moo.flot.autoscale.js
+ * autoScales y axis based on data available from min and max of x axis on draw.
+ * By @fat
+ */
+
+(function (flot) {
+    var options = {
+        series: { autoScale: null }
+    };
+
+    function init(plot) {
+
+        function autoScale(plot, s, datapoints) {
+          if (s.autoScale == null) return;
+
+          var axes = plot.getAxes();
+
+          if (!axes.yaxis.options.min) {
+
+            var _datapoints, _max = 0, _current, dataSlice = [], series = plot.getData();
+
+            for (var i = series.length; i--;) {
+              _datapoints = series[i].datapoints;
+              var j = _datapoints.points.length - 2;
+              while (j >= 0) {
+                if (_datapoints.points[j] < axes.xaxis.options.max
+                  && _datapoints.points[j] > axes.xaxis.options.min) {
+                  _current = _datapoints.points[j+1];
+                  _max = Math.max(_current, _max);
+                }
+                j-=2;
+              }
+            }
+
+            if (_max) axes.yaxis.options.max = _max + (_max * .15);
+          }
+        }
+
+        plot.hooks.processDatapoints.push(autoScale);
+    }
+
+    flot.plot.plugins.push({
+        init: init,
+        options: options,
+        name: 'autoScale',
+        version: '1.0'
+    });
+})(flot);
